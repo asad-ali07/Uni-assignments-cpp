9e3c96a1efc85c04bf747c2052c1c9121340ac24
Asad Ali <asadalirizvi116@gmail.com>
2026-04-15 17:31:45 +0500

    Task 5 personal finance tracker

---
+++ assignment 9/Task 5.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+string catergory[100];
+float amount[100];
+int transaction_count = 0;
+
+void record_transaction() {
+    cout << "Enter the transaction category: ";
+    cin >> catergory[transaction_count];
+    
+    cout << "Enter the transaction amount: ";
+    cin >> amount[transaction_count];
+
+    cout << "Transaction recorded successfully!" << endl;
+    transaction_count++;
+}
+
+float view_expenses() {
+    float total_expenses = 0;
+    for(int i = 0; i < transaction_count; i++) {
+        if(amount[i] < 0) {
+            total_expenses += -amount[i];
+        }
+    }
+    cout << "Total Expenses: $" << total_expenses << endl;
+    return total_expenses;
+}
+
+float view_income() {
+    float total_income = 0;
+    for(int i = 0; i < transaction_count; i++) {
+        if(amount[i] > 0) {
+            total_income += amount[i];
+        }
+    }
+    cout << "Total Income: $" << total_income << endl;
+    return total_income;
+}
+
+void view_budget() {
+    float expenses = view_expenses();
+    float income = view_income();
+    float budget = income + expenses;
+    cout << "Current Budget: $" << budget << endl;
+}
+
+int main() {
+    int choice;
+    cout << "Personal Finance Tracker Menu:" << endl;
+    do{
+        cout << "1. Record Transaction\n2. View Expenses\n3. View Income\n4. View Budget\n5. Exit" << endl;
+        cout << "\nChoose an option(1,2,3,4,5): ";
+        cin >> choice;
+
+        switch(choice) {
+            case 1:
+                record_transaction();
+                cout << "\n";
+                break;
+            
+            case 2:
+                cout << "Total Expenses: $" << view_expenses() << endl;
+                cout << "\n";
+                break;
+            case 3:
+                cout << "Total Income: $" << view_income() << endl;
+                cout << "\n";
+                break;
+            case 4:
+                view_budget();
+                cout << "\n";
+                break;
+            case 5:
+                cout << "Exiting the Personal Finance Tracker.\nThank you for using the program.\nGoodbye!" << endl;
+                break;
+            default:
+                cout << "Invalid option! Please choose a valid option." << endl;
+                break;
+        }
+
+    } while (choice != 5);
+    return 0;
+}
