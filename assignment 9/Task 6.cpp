5cfa33d9396a3c2bb523c5e2ef679e6217abc132
Asad Ali <asadalirizvi116@gmail.com>
2026-04-15 16:58:53 +0500

    Changed function name

--- assignment 9/Task 6.cpp
+++ assignment 9/Task 6.cpp
@@ -2,37 +2,7 @@
 #include <cmath>
 using namespace std;
 
-// float formula(int i, int a) {
-//     int radius;
-//     const float pi = 3.14;
-//     float angle = 2 * pi * i / a;
-//     float x = radius * cos(angle);
-//     float y = radius * sin(angle);
-
-//     i = x;
-//     a = y;
-//     return x,y;
-// }
-
-// int main() {
-//     int points, radius;
-//     float x, y;
-
-//     cout << "Enter the radius of the circle: ";
-//     cin >> radius;
-
-//     cout << "Enter the number of points: ";
-//     cin >> points;
-
-//     cout << "Coordinates along the circumference of the circle:" << endl;
-
-//     for(int i = 0; i < points; i++) {
-//         cout << "Point " << i + 1 << ": " << formula(i, points) << endl;
-//     }
-//     return 0;
-// }
-
-void function() {
+void numpoints() {
     int radius;
     const float pi = 3.14;
     int points;
@@ -43,7 +13,7 @@ void function() {
     cout << "Enter the number of points: ";
     cin >> points;
 
-    cout << "Coordinates along the circumference of the circle:" << endl;
+    cout << "Coordinates of points along the circumference of the circle:" << endl;
 
     for(int i = 0; i < points; i++) {
         float angle = 2 * pi * i / points;
@@ -55,6 +25,6 @@ void function() {
 }
 
 int main() {
-    function();
+    numpoints();
     return 0;
 }
